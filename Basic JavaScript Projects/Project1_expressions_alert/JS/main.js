93be383d1f700a075476fa73f02ff766f7932754
kanav-tech <your-kanav.bajaj21@gmail.com>
2025-07-25 14:34:00 -0400

    Initial commit for Project1_expressions_alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+// This variable stores the first part of the sentence
+var Sent1 = "This is the beginning of the string";
+
+// This variable stores the second part of the sentence
+var Sent2 = " and this is the end of the string";
+
+// This line combines both string variables into one and writes it to the web page
+document.write(Sent1 + Sent2);
+
+// This will display a pop-up alert with a message
+window.alert("Welcome to my JavaScript project!");
+
+// This is a math expression that multiplies two numbers and stores the result
+var result = 8 * 7;
+
+// This writes the result of the expression below the string
+document.write("<br>The result of 8 * 7 is: " + result);
